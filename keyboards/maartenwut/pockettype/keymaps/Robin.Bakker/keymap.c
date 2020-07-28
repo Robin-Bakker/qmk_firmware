fd0fcaaad1d7395b0d0f80d524d9eae125c26f4e
Robin Bakker <mail@rsbakker.nl>
2020-07-29 00:21:00 +0200

    placeholder macro code

--- keyboards/maartenwut/pockettype/keymaps/Robin.Bakker/keymap.c
+++ keyboards/maartenwut/pockettype/keymaps/Robin.Bakker/keymap.c
@@ -15,6 +15,26 @@
  */
 #include QMK_KEYBOARD_H
 
+enum my_keycodes {
+  QMKBEST = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case QMKBEST:
+        if (record->event.pressed) {
+            // when keycode QMKBEST is pressed
+            SEND_STRING("QMK is the best thing ever!");
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
+    }
+    return true;
+};
+
+
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -29,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC,  KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_DEL,
   KC_TAB,   KC_A,        KC_S,        KC_D,        KC_F,        KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-  KC_LCTL,  KC_LGUI,     KC_LALT,     KC_RALT,     MO(_LO),     KC_SPC,   KC_BSPC,  MO(_HI),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_MPLY),
+  QMKBEST,  KC_LGUI,     KC_LALT,     KC_RALT,     MO(_LO),     KC_SPC,   KC_BSPC,  MO(_HI),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_MPLY),
 
 [_LO] = LAYOUT_ortho_4x12(
   KC_F1,    KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
@@ -45,7 +65,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-enum my_keycodes {
-  FOO = SAFE_RANGE,
-  BAR
-};
+
